c85291c4020c4f1147f23ef8640939f9a457a905
Vansh2204 <Vansh2204@users.noreply.github.com>
2024-06-16 22:23:13 +0530

    Pages demo

---
+++ app/about/page.js
@@ -0,0 +1,5 @@
+export default function About(){
+    return(
+        <div style={{display:'flex',justifyContent:'center',marginTop:'20%'}}>About Page</div>
+    )
+}
---
+++ app/contact/page.js
@@ -0,0 +1,5 @@
+export default function Contact(){
+    return(
+        <div style={{display:'flex',justifyContent:'center',marginTop:'20%'}}>Contact Page</div>
+    )
+}
