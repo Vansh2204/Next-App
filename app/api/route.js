a14afd1bc7e5754b634edb02e39f9e835132ee18
Vansh2204 <Vansh2204@users.noreply.github.com>
2024-06-18 21:21:07 +0530

    API

--- app/api/route.js
+++ app/api/route.js
@@ -1,9 +1,24 @@
 const fs = require('fs');
 export async function GET(request){
-  const data = fs.readFileSync('alldata/player.json','utf-8')
-  console.log(data);
-  return new Response(data)
 
+  let data = await fs.promises.readdir("alldata");   //read directory
+  let myfile;
+  let allblogs = [];
+  
+  for(let i=0;i<data.length;i++){     //loop to iterate through alldata
+    const item = data[i];            // data[i] is filename and that is stored in item
+    myfile = await fs.promises.readFile(('alldata/'+ item),'utf-8')  //file name is read with item related 
+    console.log(myfile);
+    
+    allblogs.push(myfile)
+  }
+  return new Response(allblogs); 
+
+
+
+  // const data = fs.readFileSync('alldata/Virat.json','utf-8')
+  // console.log(data);
+  // return new Response(data)
 
 }
 
--- app/blogs/page.js
+++ app/blogs/page.js
@@ -1,9 +1,20 @@
-import Link from "next/link";
+'use client'
 
-export default function Blog(){
-    return(
-        <>
-        <div className="flex justify-center mt-8 grid grid-rows-6 grid-flow-col lg:grid-rows-2 gap-6">
+import Link from "next/link";
+const { useState, useEffect } = require("react");
+
+export default function Blog() {
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api").then((res) => res.json()).then((res) => console.log(res))
+})
+
+
+
+
+  return (
+    <>
+      <div className="flex justify-center mt-8 grid grid-rows-6 grid-flow-col lg:grid-rows-2 gap-6">
         <div className="container bg-white text-black w-64 h-48 text-center font-mono rounded-md cursor-pointer">
           <h1 className="text-2xl mt-16">Sourabh Netrawalkar</h1>
           <p className="text-sm">USA Cricket</p>
@@ -28,9 +39,9 @@ export default function Blog(){
           <h1 className="text-2xl mt-16">Fazalhaq Farooqi</h1>
           <p className="text-sm">Cricket Afghanistan</p>
         </div>
-      
+
 
       </div>
-        </>
-    )
+    </>
+  )
 }
